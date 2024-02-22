8636166710eb2203a8e28f795c55f9dcccd5ea01
RianAina <hei.rianaina@gmail.com>
2024-02-22 12:33:19 +0300

    Adding endpoint

---
+++ poja-sarisary-std22074/src/main/java/hei/school/sarisary/endpoint/LaSaryEndpoint.java
@@ -0,0 +1,14 @@
+public String transformImage(String id, MultiplepartFileimage) throws IOException {
+    String suffix = "." + FilenameUtils.getExtension(image.getOriginalFilename());
+    String prefixOriginal = id + "-initial";
+    String prefixTransformed = id + "-grayscaled";
+    String bucketKeyOriginal = directory + prefixOriginal + suffix;
+    String bucketKeyTransformed = directory + prefixTransformed + suffix ;
+
+    File toUpload = File.createTempFile(prefixOriginal, suffix);
+    image.transfertTo (toUpload);
+    File grayScaled = this.toGrayScale(id, toUpload)
+
+
+        bucketComponment
+        }
